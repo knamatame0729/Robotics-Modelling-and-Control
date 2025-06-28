f0191b02066f1d7102029e31534746a7599b7efc
kaito <kaito@kaito-ubuntu24.04>
2025-06-27 21:16:00 -0700

    Update 2nd_order_step_response_error.py

--- 2nd_order_step_response_error.py
+++ 2nd_order_step_response_error.py
@@ -7,4 +7,9 @@ G1 = TransferFunction([2], [0.5, 1])
 t, y = step(G1)
 plt.plot(t, y)
 plt.title('Step Response: G1(s)=2/(0.5s+!)')
+plt.xlabel('Time [s]')
+plt.ylabel('Output')
+plt.grid(True)
+plt.show()
+
 
