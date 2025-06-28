6c9b12546d8d08a47086322fc7957561f345beea
kaito <kaito@kaito-ubuntu24.04>
2025-06-27 21:12:40 -0700

    2nd_order_step_response_error.py

---
+++ 2nd_order_step_response_error.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.signal import TransferFunction, step
+import matplotlib.pyplot as plt
+
+# First-order example: K=2, tau=0.5
+G1 = TransferFunction([2], [0.5, 1])
+t, y = step(G1)
+plt.plot(t, y)
+plt.title('Step Response: G1(s)=2/(0.5s+!)')
+
